054da861d587e9c74e48f76482ceec2f41f4a57b
piaoruiqing <piaoruiqing@gmail.com>
2018-04-21 00:03:18 +0800

    [MOD] db config

---
+++ ez-framework-common/src/main/java/org/ez/common/dao/DemoRepository.java
@@ -0,0 +1,10 @@
+package org.ez.common.dao;
+
+import org.ez.common.entity.DemoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DemoRepository extends JpaRepository<DemoEntity, Long> {
+
+}
